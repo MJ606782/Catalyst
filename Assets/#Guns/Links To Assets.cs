79fabe1d71b331e75bee57f954db0f371b18d349
rajesh-puripanda <rajesh.puripanda@icloud.com>
2026-06-08 11:09:46 +0530

    feat: add #Guns folder

---
+++ Assets/#Guns/Links To Assets.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinksToAssets : MonoBehaviour
+{
+    //// SCAR https://sketchfab.com/3d-models/scar-l-805ef0f5325c4e468c42159607cf79b7
+    /// M16  https://assetstore.unity.com/packages/3d/props/weapons/m16-a1-rifle-182512
+    /// SILENCER https://assetstore.unity.com/packages/3d/props/guns/yhm-phantom-assault-rifle-silencer-5-56-for-free-114251
+    /// SNIPER https://assetstore.unity.com/packages/3d/props/guns/affordable-assets-sniper-rifle-69654
+    /// PISTOL https://assetstore.unity.com/packages/3d/props/guns/andoxia-modern-gun-pistol-1k-2k-4k-textures-105539
+    /// 
+    /// 
+    /// 
+    ///
+
+    // 
+}
